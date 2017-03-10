8334f372280422647e739c501731155591d08ec1
Hannah ElArabawy <helarabawy@ucla.edu>
2017-03-09 17:56:54 -0800

    Implemented some functions

--- MyMap.h
+++ MyMap.h
@@ -6,25 +6,76 @@
 template<typename KeyType, typename ValueType>
 class MyMap
 {
-public:
-	MyMap();
-	~MyMap();
-	void clear();
-	int size() const;
-	void associate(const KeyType& key, const ValueType& value);
+	public:
+		
+		// CONSTRUCTOR
+		MyMap(): m_size(0), root(nullptr){}
+		
+		// DESTRUCTOR
+		~MyMap() {clear();}
+		
+		// CLEAR MAP
+		void clear()
+		{
+			destroyTree(root);
+			m_size = 0;
+		}
+		
+		// RETURN SIZE
+		int size() const {return m_size;}
+		
+		// INSERT INTO/MODIFY TREE
+		void associate(const KeyType& key, const ValueType& value)
+		{
+			ValueType* val = find(key);
+			if (val != nullptr) // key already exists
+				*val = value;
+			else
+				insert(key, value);
+		}
+		
+		// RETURN A POINTER TO CONST ValueType (unmodifiable ValueType)
+		const ValueType* find(const KeyType& key) const;
 
-	  // for a map that can't be modified, return a pointer to const ValueType
-	const ValueType* find(const KeyType& key) const;
+		// RETURN A POINTER TO MODIFIABLE ValueType (modifiable ValueType)
+		ValueType* find(const KeyType& key)
+		{
+			return const_cast<ValueType*>(const_cast<const MyMap*>(this)->find(key));
+		}
 
-	  // for a modifiable map, return a pointer to modifiable ValueType
-	ValueType* find(const KeyType& key)
-	{
-		return const_cast<ValueType*>(const_cast<const MyMap*>(this)->find(key));
-	}
+		 // C++11 syntax for preventing copying and assignment
+		MyMap(const MyMap&) = delete;
+		MyMap& operator=(const MyMap&) = delete;
 
-	  // C++11 syntax for preventing copying and assignment
-	MyMap(const MyMap&) = delete;
-	MyMap& operator=(const MyMap&) = delete;
+	private:
+		int m_size;
+		
+		// NODE DEFINITION
+		struct Node {
+			Node* left;
+			Node* right;
+			KeyType key;
+			ValueType value;
+		};
 
-private:
+		// NODE ROOT
+		Node* root;
+
+		// DESTROY TREE
+		void destroyTree(Node* leaf)
+		{
+			if (leaf != nullptr)
+			{
+				destroyTree(leaf->left);
+				destroyTree(leaf->right);
+				delete leaf;
+			}
+		}
+
+		// INSERT INTO TREE
+		void insert(KeyType key, ValueType value)
+		{
+			// TODO INSERT IN BST TREE
+		}
+
 };
